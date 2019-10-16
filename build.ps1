273d26fa79c03aaa1a4c530c35f022e253376818
Tinylit <tinylit@foxmail.com>
2019-10-16 12:57:24 +0800

    Continuous integration.

--- build.ps1
+++ build.ps1
@@ -1,71 +1,14 @@
-[CmdletBinding(PositionalBinding=$false)]
-param(
-    [bool] $CreatePackages,
-    [bool] $RunTests = $true,
-    [string] $PullRequestNumber
-)
+<#
+    .SYNOPSIS
+        Set all installed instances of SQL server to dynamic ports
+#>
+Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\' |
+    Where-Object {
+        $_.Name -imatch 'MSSQL[_\d]+\.SQL.*'
+    } |
+    ForEach-Object {
 
-Write-Host "Run Parameters:" -ForegroundColor Cyan
-Write-Host "  CreatePackages: $CreatePackages"
-Write-Host "  RunTests: $RunTests"
-Write-Host "  dotnet --version:" (dotnet --version)
-
-$packageOutputFolder = "$PSScriptRoot\.nupkgs"
-$projectsToBuild =
-    'Dapper',
-    'Dapper.StrongName',
-    'Dapper.Contrib',
-    'Dapper.EntityFramework',
-    'Dapper.EntityFramework.StrongName',
-    'Dapper.Rainbow',
-    'Dapper.SqlBuilder'
-
-$testsToRun =
-    'Dapper.Tests',
-    'Dapper.Tests.Contrib'
-
-if ($PullRequestNumber) {
-    Write-Host "Building for a pull request (#$PullRequestNumber), skipping packaging." -ForegroundColor Yellow
-    $CreatePackages = $false
-}
-
-Write-Host "Restoring all projects..." -ForegroundColor "Magenta"
-dotnet restore
-Write-Host "Done restoring." -ForegroundColor "Green"
-
-Write-Host "Building all projects..." -ForegroundColor "Magenta"
-dotnet build -c Release --no-restore /p:CI=true
-Write-Host "Done building." -ForegroundColor "Green"
-
-if ($RunTests) {
-    foreach ($project in $testsToRun) {
-        Write-Host "Running tests: $project (all frameworks)" -ForegroundColor "Magenta"
-        Push-Location ".\$project"
-
-        dotnet test -c Release
-        if ($LastExitCode -ne 0) {
-            Write-Host "Error with tests, aborting build." -Foreground "Red"
-            Pop-Location
-            Exit 1
-        }
-
-        Write-Host "Tests passed!" -ForegroundColor "Green"
-	    Pop-Location
+        Write-Host "Setting $((Get-ItemProperty $_.PSPath).'(default)') to dynamic ports"
+        Set-ItemProperty (Join-Path $_.PSPath 'mssqlserver\supersocketnetlib\tcp\ipall') -Name TcpDynamicPorts -Value '0'
+        Set-ItemProperty (Join-Path $_.PSPath 'mssqlserver\supersocketnetlib\tcp\ipall') -Name TcpPort -Value ([string]::Empty)
     }
-}
-
-if ($CreatePackages) {
-    mkdir -Force $packageOutputFolder | Out-Null
-    Write-Host "Clearing existing $packageOutputFolder..." -NoNewline
-    Get-ChildItem $packageOutputFolder | Remove-Item
-    Write-Host "done." -ForegroundColor "Green"
-
-    Write-Host "Building all packages" -ForegroundColor "Green"
-
-    foreach ($project in $projectsToBuild) {
-        Write-Host "Packing $project (dotnet pack)..." -ForegroundColor "Magenta"
-        dotnet pack ".\$project\$project.csproj" --no-build -c Release /p:PackageOutputPath=$packageOutputFolder /p:NoPackageAnalysis=true /p:CI=true
-        Write-Host ""
-    }
-}
-Write-Host "Build Complete." -ForegroundColor "Green"
