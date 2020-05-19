77572c0959ef8dabe0c17bf0de2e7230fc342538
CN <tinylit@foxmail.com>
2020-05-19 11:22:36 +0800

    ORM 链接维护。在指定心跳时间内不关闭链接，维持链接的有效性，投入二次使用，节省链接开销，提高执行效率。

--- src/CodeArts.ORM/DbConnection.cs
+++ src/CodeArts.ORM/DbConnection.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Threading;
 
 namespace CodeArts.ORM
@@ -9,13 +11,221 @@ namespace CodeArts.ORM
     /// </summary>
     public class DbConnection : IDbConnection
     {
+        /// <summary>
+        /// 命令。
+        /// </summary>
+        private class DbCommand : IDbCommand
+        {
+            private readonly IDbCommand command;
+            private readonly List<IDataReader> dataReaders = new List<IDataReader>();
+
+            public DbCommand(IDbCommand command)
+            {
+                this.command = command ?? throw new ArgumentNullException(nameof(command));
+            }
+
+            /// <summary>
+            /// 连接。
+            /// </summary>
+            public IDbConnection Connection
+            {
+                get => command.Connection;
+                set
+                {
+                    if (command.Connection is DbConnection connection)
+                    {
+                        connection.Remove(this);
+                    }
+
+                    if (value is DbConnection db)
+                    {
+                        db.Add(this);
+                    }
+                }
+            }
+
+            /// <summary>
+            /// 事务。
+            /// </summary>
+            public IDbTransaction Transaction { get => command.Transaction; set => command.Transaction = value; }
+
+            /// <summary>
+            /// 命令。
+            /// </summary>
+            public string CommandText { get => command.CommandText; set => command.CommandText = value; }
+
+            /// <summary>
+            /// 命令超时时间。
+            /// </summary>
+            public int CommandTimeout { get => command.CommandTimeout; set => command.CommandTimeout = value; }
+
+            /// <summary>
+            /// 命令类型。
+            /// </summary>
+            public CommandType CommandType { get => command.CommandType; set => command.CommandType = value; }
+
+            /// <summary>
+            /// 参数。
+            /// </summary>
+            public IDataParameterCollection Parameters => command.Parameters;
+            /// <summary>
+            /// 获取或设置命令结果在由 System.Data.Common.DbDataAdapter 的 System.Data.IDataAdapter.Update(System.Data.DataSet) 方法使用时应用于 System.Data.DataRow 的方式。
+            /// </summary>
+            public UpdateRowSource UpdatedRowSource { get => command.UpdatedRowSource; set => command.UpdatedRowSource = value; }
+
+            /// <summary>
+            /// 取消指令。
+            /// </summary>
+            public void Cancel() => command.Cancel();
+
+            /// <summary>
+            /// 创建参数。
+            /// </summary>
+            /// <returns></returns>
+            public IDbDataParameter CreateParameter() => command.CreateParameter();
+
+            /// <summary>
+            /// 释放资源。
+            /// </summary>
+            public void Dispose()
+            {
+                dataReaders.Clear();
+
+                if (Connection is DbConnection connection)
+                {
+                    connection.Remove(this);
+                }
+
+                command.Dispose();
+            }
+
+            /// <summary>
+            /// 执行，返回影响行。
+            /// </summary>
+            /// <returns></returns>
+            public int ExecuteNonQuery() => command.ExecuteNonQuery();
+
+            /// <summary>
+            /// 存活的。
+            /// </summary>
+            public bool IsAlive => !dataReaders.TrueForAll(x => x.IsClosed);
+
+            /// <summary>
+            /// 执行并生成读取器。
+            /// </summary>
+            /// <returns></returns>
+            public IDataReader ExecuteReader()
+            {
+                dataReaders.RemoveAll(x => x.IsClosed);
+
+                var reader = command.ExecuteReader();
+
+                dataReaders.Add(reader);
+
+                return reader;
+            }
+
+            /// <summary>
+            /// 执行并生成读取器。
+            /// </summary>
+            /// <returns></returns>
+            public IDataReader ExecuteReader(CommandBehavior behavior)
+            {
+                dataReaders.RemoveAll(x => x.IsClosed);
+
+                var reader = command.ExecuteReader(behavior);
+
+                dataReaders.Add(reader);
+
+                return reader;
+            }
+
+            /// <summary>
+            /// 执行返回首行首列。
+            /// </summary>
+            /// <returns></returns>
+            public object ExecuteScalar() => command.ExecuteScalar();
+
+            /// <summary>
+            /// 准备就绪。
+            /// </summary>
+            public void Prepare() => command.Prepare();
+        }
+        /// <summary>
+        /// 事务。
+        /// </summary>
+        private class DbTransaction : IDbTransaction
+        {
+            private readonly IDbTransaction transaction;
+
+            /// <summary>
+            /// 构造函数。
+            /// </summary>
+            /// <param name="transaction"></param>
+            public DbTransaction(IDbTransaction transaction)
+            {
+                this.transaction = transaction;
+            }
+
+            public IDbConnection Connection => transaction.Connection;
+
+            public IsolationLevel IsolationLevel => transaction.IsolationLevel;
+
+            public void Commit()
+            {
+                transaction.Commit();
+
+                if (Connection is DbConnection connection)
+                {
+                    connection.Remove(this);
+                }
+            }
+
+            public void Dispose()
+            {
+                transaction.Dispose();
+
+                if (Connection is DbConnection connection)
+                {
+                    connection.Remove(this);
+                }
+
+                GC.SuppressFinalize(this);
+            }
+
+            public void Rollback()
+            {
+                transaction.Rollback();
+
+                if (Connection is DbConnection connection)
+                {
+                    connection.Remove(this);
+                }
+            }
+        }
+
+        private DateTime lastActiveTime;
         private readonly IDbConnection _connection; //数据库连接
+        private readonly double? connectionHeartbeat; //心跳
+        private readonly List<DbCommand> commands = new List<DbCommand>();
+        private readonly List<DbTransaction> transactions = new List<DbTransaction>();
 
         /// <summary>
-        /// 构造函数
+        /// 构造函数。
         /// </summary>
         /// <param name="connection">数据库链接</param>
         public DbConnection(IDbConnection connection) => _connection = connection;
+
+        /// <summary>
+        /// 构造函数。
+        /// </summary>
+        /// <param name="connection">数据库链接</param>
+        /// <param name="connectionHeartbeat">链接心跳</param>
+        public DbConnection(IDbConnection connection, double connectionHeartbeat) : this(connection)
+        {
+            lastActiveTime = DateTime.Now;
+            this.connectionHeartbeat = new double?(connectionHeartbeat);
+        }
 
         /// <summary>
         /// 数据库连接
@@ -41,14 +251,28 @@ namespace CodeArts.ORM
         /// 创建事务
         /// </summary>
         /// <returns></returns>
-        public IDbTransaction BeginTransaction() => _connection.BeginTransaction();
+        public IDbTransaction BeginTransaction()
+        {
+            var transaction = new DbTransaction(_connection.BeginTransaction());
+
+            transactions.Add(transaction);
+
+            return transaction;
+        }
 
         /// <summary>
         /// 创建事务
         /// </summary>
         /// <param name="il">隔离等级</param>
         /// <returns></returns>
-        public IDbTransaction BeginTransaction(IsolationLevel il) => _connection.BeginTransaction(il);
+        public IDbTransaction BeginTransaction(IsolationLevel il)
+        {
+            var transaction = new DbTransaction(_connection.BeginTransaction(il));
+
+            transactions.Add(transaction);
+
+            return transaction;
+        }
 
         /// <summary>
         /// 修改数据库
@@ -67,6 +291,7 @@ namespace CodeArts.ORM
                     _connection.Open();
                     break;
                 case ConnectionState.Connecting:
+
                     do
                     {
                         Thread.Sleep(5);
@@ -82,16 +307,76 @@ namespace CodeArts.ORM
 
         void IDbConnection.Close() => Close();
 
+        private void Add(DbCommand command)
+        {
+            if (!commands.Contains(command))
+            {
+                commands.Add(command);
+            }
+        }
+        private void Remove(DbCommand command)
+        {
+            commands.Remove(command);
+        }
+
+        private void Remove(DbTransaction transaction)
+        {
+            transactions.Remove(transaction);
+        }
+
         /// <summary>
         /// 关闭链接
         /// </summary>
-        public virtual void Close() => _connection.Close();
+        public virtual void Close()
+        {
+            if (State == ConnectionState.Closed)
+            {
+                return;
+            }
+
+            if (connectionHeartbeat.HasValue && DateTime.Now <= lastActiveTime.AddMinutes(connectionHeartbeat.Value))
+            {
+                return;
+            }
+
+            _connection.Close();
+        }
 
         /// <summary>
         /// 创建命令
         /// </summary>
         /// <returns></returns>
-        public IDbCommand CreateCommand() => _connection.CreateCommand();
+        public IDbCommand CreateCommand()
+        {
+            var command = new DbCommand(_connection.CreateCommand());
+
+            commands.Add(command);
+
+            lastActiveTime = DateTime.Now;
+
+            return command;
+        }
+
+        /// <summary>
+        /// 是否存活。
+        /// </summary>
+        public bool IsAlive
+        {
+            get
+            {
+                if (connectionHeartbeat.HasValue)
+                {
+                    return State == ConnectionState.Open && DateTime.Now <= lastActiveTime.AddMinutes(connectionHeartbeat.Value);
+                }
+
+                return State == ConnectionState.Open;
+            }
+        }
+
+        /// <summary>
+        /// 是否闲置。
+        /// </summary>
+        public bool IsIdle => transactions.Count == 0 && commands.TrueForAll(x => !x.IsAlive);
 
         /// <summary>
         /// 释放器不释放
--- src/CodeArts.ORM/DispatchConnections.cs
+++ src/CodeArts.ORM/DispatchConnections.cs
@@ -1,4 +1,7 @@
-﻿using System.Data;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using Timer = System.Timers.Timer;
 
 namespace CodeArts.ORM
 {
@@ -19,6 +22,56 @@ namespace CodeArts.ORM
         /// </summary>
         private class DefaultConnections : IDispatchConnections
         {
+            private bool _clearTimerRun;
+            private readonly Timer _clearTimer;
+            private readonly ConcurrentDictionary<string, List<DbConnection>> connectionCache = new ConcurrentDictionary<string, List<DbConnection>>();
+
+            public DefaultConnections()
+            {
+                _clearTimer = new Timer(1000 * 60);
+                _clearTimer.Elapsed += ClearTimerElapsed;
+                _clearTimer.Enabled = true;
+                _clearTimer.Stop();
+                _clearTimerRun = false;
+            }
+
+            private void ClearTimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+            {
+                var list = new List<string>();
+                foreach (var kv in connectionCache)
+                {
+                    kv.Value.RemoveAll(x =>
+                    {
+                        if (x.IsAlive)
+                        {
+                            return false;
+                        }
+
+                        x.Dispose();
+
+                        return true;
+                    });
+
+                    if (kv.Value.Count == 0)
+                    {
+                        list.Add(kv.Key);
+                    }
+                }
+
+                list.ForEach(key =>
+                {
+                    if (connectionCache.TryRemove(key, out List<DbConnection> connections) && connections.Count > 0)
+                    {
+                        connectionCache.TryAdd(key, connections);
+                    }
+                });
+
+                if (connectionCache.Count == 0)
+                {
+                    _clearTimerRun = false;
+                    _clearTimer.Stop();
+                }
+            }
             /// <summary>
             /// 链接管理
             /// </summary>
@@ -26,7 +79,30 @@ namespace CodeArts.ORM
             /// <param name="adapter">适配器</param>
             /// <param name="useCache">是否使用缓存</param>
             /// <returns></returns>
-            public IDbConnection Create(string connectionString, IDbConnectionAdapter adapter, bool useCache = true) => adapter.Create(connectionString);
+            public IDbConnection Create(string connectionString, IDbConnectionAdapter adapter, bool useCache = true)
+            {
+                var connections = connectionCache.GetOrAdd(connectionString, _ => new List<DbConnection>());
+
+                foreach (var item in connections)
+                {
+                    if (item.IsAlive && item.IsIdle)
+                    {
+                        return item;
+                    }
+                }
+
+                var connection = new DbConnection(adapter.Create(connectionString), adapter.ConnectionHeartbeat);
+
+                connections.Add(connection);
+
+                if (!_clearTimerRun)
+                {
+                    _clearTimer.Start();
+                    _clearTimerRun = true;
+                }
+
+                return connection;
+            }
         }
 
         /// <summary>
