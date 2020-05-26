2582de3a76aa3ee3cbda412d4f888e4f4f72411a
CN <tinylit@foxmail.com>
2020-05-26 17:41:30 +0800

    依赖注入SQL参数空或空字符处理。

--- src/CodeArts.ORM.DependencyInjection/DbAnnotations/SqlAttribute.cs
+++ src/CodeArts.ORM.DependencyInjection/DbAnnotations/SqlAttribute.cs
@@ -1,6 +1,4 @@
-﻿using CodeArts.ORM;
-using System;
-using System.Linq;
+﻿using System;
 
 namespace CodeArts.DbAnnotations
 {
@@ -17,6 +15,16 @@ namespace CodeArts.DbAnnotations
         /// <param name="commandType">命令类型</param>
         public SqlAttribute(string sql, UppercaseString commandType)
         {
+            if (sql is null)
+            {
+                throw new ArgumentNullException(nameof(sql));
+            }
+
+            if (sql.Length == 0)
+            {
+                throw new ArgumentException(nameof(sql));
+            }
+
             Sql = sql;
             CommandType = commandType;
         }
