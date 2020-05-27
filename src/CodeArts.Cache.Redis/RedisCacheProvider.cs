a4318655d3186bdd45e39caebda20570024d5913
CN <tinylit@foxmail.com>
2020-05-27 09:16:43 +0800

    提供全局Redis供应器链接设置。

--- src/CodeArts.Cache.Redis/RedisCacheProvider.cs
+++ src/CodeArts.Cache.Redis/RedisCacheProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 
 namespace CodeArts.Cache
 {
@@ -7,6 +8,8 @@ namespace CodeArts.Cache
     /// </summary>
     public class RedisCacheProvider : ICacheProvider
     {
+        private readonly int defaultDb;
+        private readonly string connectString;
         private static readonly ConcurrentDictionary<string, ICache> Caches;
 
         /// <summary>
@@ -15,10 +18,26 @@ namespace CodeArts.Cache
         static RedisCacheProvider() => Caches = new ConcurrentDictionary<string, ICache>();
 
         /// <summary>
+        /// 构造函数
+        /// </summary>
+        public RedisCacheProvider(int defaultDb = 0) : this("redis".Config<string>(), defaultDb)
+        {
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public RedisCacheProvider(string connectString, int defaultDb = 0)
+        {
+            this.connectString = connectString ?? throw new ArgumentNullException(nameof(connectString));
+            this.defaultDb = defaultDb;
+        }
+
+        /// <summary>
         /// 获取缓存对象
         /// </summary>
         /// <param name="regionName">缓存区域名称</param>
         /// <returns></returns>
-        public ICache GetCache(string regionName) => Caches.GetOrAdd(regionName, name => new RedisCache(name));
+        public ICache GetCache(string regionName) => Caches.GetOrAdd(regionName, name => new RedisCache(name, connectString, defaultDb));
     }
 }
